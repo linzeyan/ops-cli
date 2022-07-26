b62b03167030bbcd3ed3ab99c85593ac15300cf8
Ricky <zeyanlin@outlook.com>
2022-07-26 13:51:14 +0800

    Modify release function

--- build.bash
+++ build.bash
@@ -9,16 +9,16 @@ build() {
 
 release() {
     export CGO_ENABLED=0 GOOS=darwin GOARCH=amd64
-    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)' -X 'github.com/linzeyan/ops-cli/cmd.appPlatform=${GOOS}/${GOARCH}'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
+    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
 
     export CGO_ENABLED=0 GOOS=linux GOARCH=amd64
-    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)' -X 'github.com/linzeyan/ops-cli/cmd.appPlatform=${GOOS}/${GOARCH}'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
+    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
 
     export CGO_ENABLED=0 GOOS=linux GOARCH=arm64
-    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)' -X 'github.com/linzeyan/ops-cli/cmd.appPlatform=${GOOS}/${GOARCH}'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
+    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
 
     export CGO_ENABLED=0 GOOS=windows GOARCH=amd64
-    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)' -X 'github.com/linzeyan/ops-cli/cmd.appPlatform=${GOOS}/${GOARCH}'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
+    go build -a -trimpath -ldflags="-X 'github.com/linzeyan/ops-cli/cmd.appVersion=$(git describe --tags)' -X 'github.com/linzeyan/ops-cli/cmd.appBuildTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')' -X 'github.com/linzeyan/ops-cli/cmd.appCommit=$(git rev-parse HEAD)'" -o $(basename ${PWD})_${GOOS}_${GOARCH} .
 }
 
 version() {
