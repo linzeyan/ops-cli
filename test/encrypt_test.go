33b0bcf3327011ff2cda1e1840e8d286e57175c2
Ricky <zeyanlin@outlook.com>
2022-08-18 10:20:39 +0800

    [test]: Add test cases

---
+++ test/encrypt_test.go
@@ -0,0 +1,45 @@
+package test_test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncrypt(t *testing.T) {
+	const subCommand = "encrypt"
+	testCases := []struct {
+		input    []string
+		expected string
+	}{
+		{[]string{runCommand, mainGo, subCommand, "aes", "-f", "../.gitignore", "-k", "32449939618748684094059431382108"}, "../.gitignore"},
+		{[]string{runCommand, mainGo, subCommand, "aes", "-f", "../.gitignore", "-k", "32449939618748684094059431382108", "-d"}, "../.gitignore"},
+	}
+
+	for i := range testCases {
+		t.Run(testCases[i].input[3], func(t *testing.T) {
+			err := exec.Command(mainCommand, testCases[i].input...).Run()
+			if err != nil {
+				t.Error(err)
+			}
+			assert.FileExists(t, testCases[i].expected)
+		})
+	}
+}
+
+func TestBinaryEncrypt(t *testing.T) {
+	const subCommand = "encrypt"
+	args := [][]string{
+		{subCommand, "aes", "-f", "../.gitignore", "-k", "32449939618748684094059431382108"},
+		{subCommand, "aes", "-f", "../.gitignore", "-k", "32449939618748684094059431382108", "-d"},
+	}
+
+	for i := range args {
+		t.Run(args[i][1], func(t *testing.T) {
+			if err := exec.Command(binaryCommand, args[i]...).Run(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
