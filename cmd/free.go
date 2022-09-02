60cb8260826e1ed409450f46c7422054c007c72b
Ricky <zeyanlin@outlook.com>
2022-09-02 14:27:32 +0800

    [chore]: Update free

--- cmd/free.go
+++ cmd/free.go
@@ -19,6 +19,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/linzeyan/ops-cli/cmd/common"
 	"github.com/olekukonko/tablewriter"
@@ -29,44 +30,74 @@ import (
 var freeCmd = &cobra.Command{
 	Use:   CommandFree,
 	Short: "Display free memory spaces",
-	RunE: func(_ *cobra.Command, _ []string) error {
-		var err error
-		swap, err := mem.SwapMemory()
-		if err != nil {
-			return err
-		}
-		memory, err := mem.VirtualMemory()
-		if err != nil {
-			return err
-		}
-		var header = []string{"", "total", "used", "free", "available", "use%"}
-		var data [][]string
-		data = append(data, []string{
-			"Mem:",
-			common.ByteSize(memory.Total).String(),
-			common.ByteSize(memory.Used).String(),
-			common.ByteSize(memory.Free).String(),
-			common.ByteSize(memory.Available).String(),
-			fmt.Sprintf("%.2f%%", memory.UsedPercent),
-		})
-		data = append(data, []string{
-			"Swap:",
-			common.ByteSize(swap.Total).String(),
-			common.ByteSize(swap.Used).String(),
-			common.ByteSize(swap.Free).String(),
-			"",
-			fmt.Sprintf("%.2f%%", swap.UsedPercent),
-		})
-		FreeTable(header, data)
-		return err
-	},
+	RunE:  freeCmdGlobalVar.RunE,
 }
+
+var freeCmdGlobalVar FreeFlag
 
 func init() {
 	rootCmd.AddCommand(freeCmd)
+	freeCmd.Flags().UintVarP(&freeCmdGlobalVar.count, "count", "c", 0, "Repeat printing times")
+	freeCmd.Flags().UintVarP(&freeCmdGlobalVar.second, "seconds", "s", 0, "Seconds between each repeat printing")
 }
 
-func FreeTable(header []string, data [][]string) {
+type FreeFlag struct {
+	count  uint
+	second uint
+}
+
+func (f *FreeFlag) RunE(_ *cobra.Command, _ []string) error {
+	var err error
+	if f.count == 0 && f.second == 0 {
+		return f.Output()
+	}
+	var counter uint
+	for {
+		err = f.Output()
+		if err != nil {
+			return err
+		}
+		counter++
+		if f.count > 0 && f.count == counter {
+			return err
+		}
+		time.Sleep(time.Second * time.Duration(f.second))
+	}
+}
+
+func (f *FreeFlag) Output() error {
+	var err error
+	swap, err := mem.SwapMemory()
+	if err != nil {
+		return err
+	}
+	memory, err := mem.VirtualMemory()
+	if err != nil {
+		return err
+	}
+	var header = []string{"", "total", "used", "free", "available", "use%"}
+	var data [][]string
+	data = append(data, []string{
+		"Mem:",
+		common.ByteSize(memory.Total).String(),
+		common.ByteSize(memory.Used).String(),
+		common.ByteSize(memory.Free).String(),
+		common.ByteSize(memory.Available).String(),
+		fmt.Sprintf("%.2f%%", memory.UsedPercent),
+	})
+	data = append(data, []string{
+		"Swap:",
+		common.ByteSize(swap.Total).String(),
+		common.ByteSize(swap.Used).String(),
+		common.ByteSize(swap.Free).String(),
+		"",
+		fmt.Sprintf("%.2f%%", swap.UsedPercent),
+	})
+	f.String(header, data)
+	return err
+}
+
+func (FreeFlag) String(header []string, data [][]string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader(header)
 	table.SetAutoWrapText(false)
