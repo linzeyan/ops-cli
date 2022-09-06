48a16bfc88e0ac16c2760981a115905382404fa9
Ricky <zeyanlin@outlook.com>
2022-09-06 11:23:04 +0800

    [feat]: Add date

---
+++ cmd/date.go
@@ -0,0 +1,103 @@
+/*
+Copyright © 2022 ZeYanLin <zeyanlin@outlook.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"time"
+
+	"github.com/linzeyan/ops-cli/cmd/common"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var dateFlag DateFlag
+	var dateCmd = &cobra.Command{
+		Use:       CommandDate,
+		ValidArgs: []string{"milli", "micro", "nano"},
+		Short:     "Print date time",
+		Run: func(_ *cobra.Command, args []string) {
+			if dateFlag.seconds {
+				dateFlag.PrintUnixTime(args)
+				return
+			}
+			if dateFlag.date {
+				PrintString(dateFlag.Now().Format("2006-01-02"))
+				return
+			}
+			if dateFlag.time {
+				PrintString(dateFlag.Now().Format("15:04:05"))
+				return
+			}
+			if dateFlag.format {
+				dateFlag.PrintFormat(args)
+				return
+			}
+			PrintString(dateFlag.Now().Format(time.RFC3339))
+		},
+	}
+	rootCmd.AddCommand(dateCmd)
+	dateCmd.Flags().BoolVarP(&dateFlag.format, "format", "f", false, common.Usage("Print date using specific format"))
+	dateCmd.Flags().BoolVarP(&dateFlag.seconds, "seconds", "s", false, common.Usage("Print Unix time"))
+	dateCmd.Flags().BoolVarP(&dateFlag.utc, "utc", "u", false, common.Usage("Print date using UTC time"))
+	dateCmd.Flags().BoolVarP(&dateFlag.date, "date", "D", false, common.Usage(`Print date using '2006-01-02' format`))
+	dateCmd.Flags().BoolVarP(&dateFlag.time, "time", "T", false, common.Usage("Print time using '15:04:05' format"))
+}
+
+type DateFlag struct {
+	utc     bool
+	seconds bool
+	format  bool
+	date    bool
+	time    bool
+}
+
+func (d *DateFlag) Now() time.Time {
+	if d.utc {
+		return common.TimeNow.UTC()
+	}
+	return common.TimeNow
+}
+
+func (d *DateFlag) PrintFormat(args []string) {
+	if len(args) == 0 {
+		PrintString(d.Now().Format(time.RFC3339))
+		return
+	}
+	PrintString(d.Now().Format(args[0]))
+}
+
+func (d *DateFlag) PrintUnixTime(args []string) {
+	var s = map[string]string{
+		"milli": time.StampMilli,
+		"micro": time.StampMicro,
+		"nano":  time.StampNano,
+	}
+	if len(args) == 0 {
+		PrintString(common.TimeNow.Unix())
+		return
+	}
+	switch s[args[0]] {
+	default:
+		PrintString(common.TimeNow.Unix())
+	case time.StampMilli:
+		PrintString(common.TimeNow.UnixMilli())
+	case time.StampMicro:
+		PrintString(common.TimeNow.UnixMicro())
+	case time.StampNano:
+		PrintString(common.TimeNow.UnixNano())
+	}
+}
