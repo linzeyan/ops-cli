fb6cd4313a5d057f0c9d95342deaa1d7def2cc84
Ricky <zeyanlin@outlook.com>
2022-09-15 13:55:32 +0800

    [test]: Add test cases for netmask

---
+++ test/netmask_test.go
@@ -0,0 +1,63 @@
+package test_test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/linzeyan/ops-cli/cmd"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNetmask(t *testing.T) {
+	const subCommand = cmd.CommandNetmask
+
+	testCases := []struct {
+		input    []string
+		expected string
+	}{
+		{[]string{subCommand, "-b", "3.4.5.6/7"}, "00000010 00000000 00000000 00000000 / 11111110 00000000 00000000 00000000\n"},
+		{[]string{subCommand, "-o", "3.4.5.6/7"}, "2 0 0 0 / 376 0 0 0\n"},
+		{[]string{subCommand, "-d", "3.4.5.6/7"}, "2.0.0.0 / 254.0.0.0\n"},
+		{[]string{subCommand, "-x", "3.4.5.6/7"}, "2 0 0 0 / fe 0 0 0\n"},
+		{[]string{subCommand, "-i", "3.4.5.6/7"}, "2.0.0.0 / 1.255.255.255\n"},
+		{[]string{subCommand, "-r", "3.4.5.6/7"}, "2.0.0.0 -> 3.255.255.255 (33554432)\n"},
+		{[]string{subCommand, "-c", "3.4.5.0-3.4.5.1"}, "3.4.5.0/31\n"},
+		{[]string{subCommand, "-i", "3.4.5.0-3.4.5.1"}, "3.4.5.0 / 0.0.0.1\n"},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.input[1], func(t *testing.T) {
+			got, err := exec.Command(binaryCommand, testCase.input...).Output()
+			if err != nil {
+				t.Error(testCase.input[1], err)
+			}
+			assert.Equal(t, testCase.expected, string(got))
+		})
+	}
+}
+
+func TestNetmaskBinary(t *testing.T) {
+	const subCommand = cmd.CommandNetmask
+	t.Run(subCommand, func(t *testing.T) {
+		if err := exec.Command(binaryCommand, subCommand).Run(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	args := [][]string{
+		{subCommand, "-b", "3.4.5.6/7"},
+		{subCommand, "-o", "3.4.5.6/7"},
+		{subCommand, "-d", "3.4.5.6/7"},
+		{subCommand, "-x", "3.4.5.6/7"},
+		{subCommand, "-i", "3.4.5.6/7"},
+		{subCommand, "-r", "3.4.5.6/7"},
+		{subCommand, "-c", "1.1.1.1-2.2.2.2"},
+		{subCommand, "-i", "1.1.1.1-2.2.2.2"},
+	}
+	for i := range args {
+		t.Run(args[i][1], func(t *testing.T) {
+			if err := exec.Command(binaryCommand, args[i]...).Run(); err != nil {
+				t.Error(args[i], err)
+			}
+		})
+	}
+}
