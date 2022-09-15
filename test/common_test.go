31fd658a27bb70ce4141ef148434b948285e822d
Ricky <zeyanlin@outlook.com>
2022-09-15 16:26:46 +0800

    [test]: Update common test cases

--- test/common_test.go
+++ test/common_test.go
@@ -14,11 +14,11 @@ func TestCommonBytes(t *testing.T) {
 		expected string
 	}{
 		{1, "1.00B"},
-		{1024, "1.00KB"},
-		{65530, "63.99KB"},
-		{65530000, "62.49MB"},
-		{89505395658828411324, "77.63EB"},
-		{89505395658828.411324, "81.40TB"},
+		{1024, "1.00KiB"},
+		{65530, "63.99KiB"},
+		{65530000, "62.49MiB"},
+		{89505395658828411324, "77.63EiB"},
+		{89505395658828.411324, "81.40TiB"},
 	}
 	for _, testCase := range testCases {
 		t.Run(fmt.Sprintf("%.2f", testCase.input), func(t *testing.T) {
