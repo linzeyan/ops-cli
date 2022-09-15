954bcf68c8b63a52f0e750c0a2b463df6c2211a8
Ricky <zeyanlin@outlook.com>
2022-09-15 14:01:50 +0800

    [test]: Update test cases

--- test/netmask_test.go
+++ test/netmask_test.go
@@ -15,13 +15,24 @@ func TestNetmask(t *testing.T) {
 		input    []string
 		expected string
 	}{
-		{[]string{subCommand, "-b", "3.4.5.6/7"}, "00000010 00000000 00000000 00000000 / 11111110 00000000 00000000 00000000\n"},
+		{
+			[]string{subCommand, "-b", "3.4.5.6/7"},
+			"00000010 00000000 00000000 00000000 / 11111110 00000000 00000000 00000000\n",
+		},
 		{[]string{subCommand, "-o", "3.4.5.6/7"}, "2 0 0 0 / 376 0 0 0\n"},
 		{[]string{subCommand, "-d", "3.4.5.6/7"}, "2.0.0.0 / 254.0.0.0\n"},
 		{[]string{subCommand, "-x", "3.4.5.6/7"}, "2 0 0 0 / fe 0 0 0\n"},
 		{[]string{subCommand, "-i", "3.4.5.6/7"}, "2.0.0.0 / 1.255.255.255\n"},
 		{[]string{subCommand, "-r", "3.4.5.6/7"}, "2.0.0.0 -> 3.255.255.255 (33554432)\n"},
+		{
+			[]string{subCommand, "-r", "fe80::aede:48ff:fe00:1122/64"},
+			"fe80:: -> fe80::ffff:ffff:ffff:ffff (18446744073709551616)\n",
+		},
 		{[]string{subCommand, "-c", "3.4.5.0-3.4.5.1"}, "3.4.5.0/31\n"},
+		{
+			[]string{subCommand, "-c", "fe80::aede:48ff:fe00:1122-fe80::aede:48ff:fe00:112a"},
+			"fe80::aede:48ff:fe00:1122/127\nfe80::aede:48ff:fe00:1124/126\nfe80::aede:48ff:fe00:1128/127\nfe80::aede:48ff:fe00:112a/128\n",
+		},
 		{[]string{subCommand, "-i", "3.4.5.0-3.4.5.1"}, "3.4.5.0 / 0.0.0.1\n"},
 	}
 	for _, testCase := range testCases {
