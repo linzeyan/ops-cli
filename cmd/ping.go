00cd6461171a988ae2cfc34e02aed3536cd77ddb
Ricky <zeyanlin@outlook.com>
2022-09-19 17:30:41 +0800

    [feat]: Add ping

---
+++ cmd/ping.go
@@ -0,0 +1,141 @@
+/*
+Copyright © 2022 ZeYanLin <zeyanlin@outlook.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/linzeyan/ops-cli/cmd/common"
+	"github.com/spf13/cobra"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+func init() {
+	var pingFlag PingFlag
+	var pingCmd = &cobra.Command{
+		Use:   "ping [host]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Send ICMP ECHO_REQUEST packets to network hosts.",
+		Run:   pingFlag.Run,
+	}
+	rootCmd.AddCommand(pingCmd)
+	pingCmd.Flags().IntVarP(&pingFlag.count, "count", "c", -1, common.Usage("Specify ping counts"))
+	pingCmd.Flags().BoolVarP(&pingFlag.icmp, "icmp", "", false, common.Usage("Use ICMP procotol (default: udp)"))
+	pingCmd.Flags().IntVarP(&pingFlag.size, "size", "s", 24, common.Usage("Specify packet size"))
+	pingCmd.Flags().IntVarP(&pingFlag.ttl, "ttl", "", 64, common.Usage("Specify packet ttl"))
+	pingCmd.Flags().DurationVarP(&pingFlag.interval, "interval", "i", time.Second, common.Usage("Specify interval"))
+	pingCmd.Flags().DurationVarP(&pingFlag.timeout, "timeout", "t", 2*time.Second, common.Usage("Specify timeout"))
+}
+
+type PingFlag struct {
+	icmp             bool
+	count, size, ttl int
+	interval         time.Duration
+	timeout          time.Duration
+}
+
+func (p *PingFlag) Run(cmd *cobra.Command, args []string) {
+	host := args[0]
+
+	header := fmt.Sprintf("PING %s (%s):", host, host)
+	PrintString(header)
+
+	for i := 0; i != p.count; i++ {
+		if err := p.Connect(i, host); err != nil {
+			PrintString(err)
+		}
+		time.Sleep(p.interval)
+	}
+}
+
+func (p *PingFlag) Connect(counter int, host string) error {
+	// var network = "udp4"
+	// if p.icmp {
+	// 	network = "ip4:icmp"
+	// }
+	conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		return err
+	}
+	if conn != nil {
+		defer conn.Close()
+	}
+	// if err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true); err != nil {
+	// 	return err
+	// }
+	// if err = conn.IPv4PacketConn().SetTTL(p.ttl); err != nil {
+	// 	return err
+	// }
+	ip, err := net.ResolveIPAddr("ip4", host)
+	if err != nil {
+		return err
+	}
+	icmpData := []byte("abcdefghijklmnopqrstuvwx")
+	data := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   os.Getpid() & 0xffff,
+			Seq:  counter,
+			Data: icmpData,
+		},
+	}
+	b, err := data.Marshal(nil)
+	if err != nil {
+		return err
+	}
+
+	/* Send packet. */
+	startTime := time.Now()
+	_, err = conn.WriteTo(b, ip)
+	if err != nil {
+		return err
+	}
+
+	/* Wait receiving. */
+	reply := make([]byte, 1500)
+	if err = conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		return err
+	}
+	n, peer, err := conn.ReadFrom(reply)
+	if err != nil {
+		e := fmt.Sprintf("Request timeout for icmp_seq %d", counter)
+		return errors.New(e)
+	}
+	duration := time.Since(startTime)
+	ttl := reply[8]
+
+	result, err := icmp.ParseMessage(1, reply[:n])
+	if err != nil {
+		return err
+	}
+	var out string
+	switch result.Type {
+	case ipv4.ICMPTypeEchoReply:
+		out = fmt.Sprintf("%v bytes from %v: icmp_seq=%d ttl=%d time=%v", len(b), peer, counter, ttl, duration)
+	case ipv4.ICMPTypeDestinationUnreachable:
+		out = fmt.Sprintf("%v Destination Unreachable", peer)
+	}
+	PrintString(out)
+
+	return err
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/stretchr/testify v1.8.0
 	github.com/tomwright/dasel v1.26.0
 	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
 	golang.org/x/text v0.3.7
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -36,7 +37,7 @@ require (
 	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.3.1 // indirect
@@ -66,7 +67,6 @@ require (
 	github.com/tklauser/numcpus v0.4.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/tools v0.1.10 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
