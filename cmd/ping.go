d7c343593ebe5ab934efda5a963fc6d86f402572
Ricky <zeyanlin@outlook.com>
2022-09-20 14:10:08 +0800

    [chore]: Support ipv6 for ping

--- cmd/ping.go
+++ cmd/ping.go
@@ -26,6 +26,7 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
 
 func init() {
@@ -38,6 +39,7 @@ func init() {
 	}
 	rootCmd.AddCommand(pingCmd)
 	pingCmd.Flags().IntVarP(&pingFlag.count, "count", "c", -1, common.Usage("Specify ping counts"))
+	pingCmd.Flags().BoolVarP(&pingFlag.ipv6, "ipv6", "6", false, common.Usage("Use ICMPv6"))
 	pingCmd.Flags().IntVarP(&pingFlag.size, "size", "s", 24, common.Usage("Specify packet size"))
 	pingCmd.Flags().IntVarP(&pingFlag.ttl, "ttl", "", 64, common.Usage("Specify packet ttl"))
 	pingCmd.Flags().DurationVarP(&pingFlag.interval, "interval", "i", time.Second, common.Usage("Specify interval"))
@@ -45,6 +47,7 @@ func init() {
 }
 
 type PingFlag struct {
+	ipv6             bool
 	count, size, ttl int
 	interval         time.Duration
 	timeout          time.Duration
@@ -66,6 +69,13 @@ func (p *PingFlag) Run(cmd *cobra.Command, args []string) {
 }
 
 func (p *PingFlag) Connect(counter int, host string, icmpData []byte) error {
+	if p.ipv6 {
+		return p.ping6(counter, host, icmpData)
+	}
+	return p.ping4(counter, host, icmpData)
+}
+
+func (p *PingFlag) ping4(counter int, host string, icmpData []byte) error {
 	conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
 		return err
@@ -73,12 +83,14 @@ func (p *PingFlag) Connect(counter int, host string, icmpData []byte) error {
 	if conn != nil {
 		defer conn.Close()
 	}
+
 	if err = conn.IPv4PacketConn().SetTTL(p.ttl); err != nil {
 		return err
 	}
 	if err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true); err != nil {
 		return err
 	}
+
 	ip, err := net.ResolveIPAddr("ip4", host)
 	if err != nil {
 		return err
@@ -115,7 +127,6 @@ func (p *PingFlag) Connect(counter int, host string, icmpData []byte) error {
 		e := fmt.Sprintf("Request timeout for icmp_seq %d", counter)
 		return errors.New(e)
 	}
-	duration := time.Since(startTime)
 
 	result, err := icmp.ParseMessage(1, reply[:n])
 	if err != nil {
@@ -124,8 +135,80 @@ func (p *PingFlag) Connect(counter int, host string, icmpData []byte) error {
 	var out string
 	switch result.Type {
 	case ipv4.ICMPTypeEchoReply:
-		out = fmt.Sprintf("%v bytes from %v: icmp_seq=%d ttl=%d time=%v", len(b), peer, counter, cm.TTL, duration)
+		out = fmt.Sprintf("%v bytes from %v: icmp_seq=%d ttl=%d time=%v", len(b), peer, counter, cm.TTL, time.Since(startTime))
 	case ipv4.ICMPTypeDestinationUnreachable:
+		out = fmt.Sprintf("%v Destination Unreachable", peer)
+	}
+	PrintString(out)
+
+	return err
+}
+
+func (p *PingFlag) ping6(counter int, host string, icmpData []byte) error {
+	conn, err := icmp.ListenPacket("ip6:ipv6-icmp", "::")
+	if err != nil {
+		return err
+	}
+	if conn != nil {
+		defer conn.Close()
+	}
+
+	if err = conn.IPv6PacketConn().SetHopLimit(p.ttl); err != nil {
+		return err
+	}
+	if err = conn.IPv6PacketConn().SetControlMessage(ipv6.FlagHopLimit, true); err != nil {
+		return err
+	}
+	if err = conn.IPv6PacketConn().SetControlMessage(ipv6.FlagInterface, true); err != nil {
+		return err
+	}
+
+	ip, err := net.ResolveIPAddr("ip6", host)
+	if err != nil {
+		return err
+	}
+
+	data := icmp.Message{
+		Type: ipv6.ICMPTypeEchoRequest,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   counter,
+			Seq:  counter,
+			Data: icmpData,
+		},
+	}
+	b, err := data.Marshal(nil)
+	if err != nil {
+		return err
+	}
+
+	/* Send packet. */
+	startTime := time.Now()
+	_, err = conn.WriteTo(b, ip)
+	if err != nil {
+		return err
+	}
+
+	/* Wait receiving. */
+	reply := make([]byte, 1500)
+	if err = conn.SetReadDeadline(time.Now().Add(p.timeout)); err != nil {
+		return err
+	}
+	n, cm, peer, err := conn.IPv6PacketConn().ReadFrom(reply)
+	if err != nil {
+		e := fmt.Sprintf("Request timeout for icmp_seq %d", counter)
+		return errors.New(e)
+	}
+
+	result, err := icmp.ParseMessage(58, reply[:n])
+	if err != nil {
+		return err
+	}
+	var out string
+	switch result.Type {
+	case ipv6.ICMPTypeEchoReply:
+		out = fmt.Sprintf("%v bytes from %v: icmp_seq=%d ttl=%d time=%v", len(b), peer, counter, cm.HopLimit, time.Since(startTime))
+	case ipv6.ICMPTypeDestinationUnreachable:
 		out = fmt.Sprintf("%v Destination Unreachable", peer)
 	}
 	PrintString(out)
