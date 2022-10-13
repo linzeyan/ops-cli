486d3241d6c1028781b80de1e399614a7cac1f9d
Ricky <zeyanlin@outlook.com>
2022-10-13 13:26:10 +0800

    [chore]: Update ssh-keygen
    Get public key from private key

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -34,6 +34,7 @@ func initSSHKeyGen() *cobra.Command {
 	var flags struct {
 		bit  int
 		path string
+		key  string
 	}
 	var sshkeygenCmd = &cobra.Command{
 		Use:   CommandSSH,
@@ -43,6 +44,9 @@ func initSSHKeyGen() *cobra.Command {
 		},
 		RunE: func(_ *cobra.Command, _ []string) error {
 			var k SSHKeygen
+			if flags.key != "" {
+				return k.GetPub(flags.key)
+			}
 			if flags.bit < 4096 {
 				flags.bit = 4096
 			}
@@ -51,13 +55,14 @@ func initSSHKeyGen() *cobra.Command {
 	}
 	sshkeygenCmd.Flags().IntVarP(&flags.bit, "bits", "b", 4096, common.Usage("Specifies the number of bits in the key to create"))
 	sshkeygenCmd.Flags().StringVarP(&flags.path, "file", "f", "id_rsa", common.Usage("Specify the file path to generate"))
+	sshkeygenCmd.Flags().StringVarP(&flags.key, "generate", "g", "", common.Usage("Get public key from private key"))
 	return sshkeygenCmd
 }
 
 type SSHKeygen struct{}
 
 /* Prepare checks file exist or not, and return files name. */
-func (r *SSHKeygen) Prepare(path string) (string, string) {
+func (*SSHKeygen) Prepare(path string) (string, string) {
 	privateKeyFile := path
 	if validator.ValidFile(privateKeyFile) {
 		PrintString(privateKeyFile + " exist.")
@@ -98,5 +103,26 @@ func (r *SSHKeygen) Generate(bit int, path string) error {
 		return err
 	}
 	PrintString(pubFile + " generated")
+	return err
+}
+
+func (*SSHKeygen) GetPub(keyFile string) error {
+	f, err := os.ReadFile(keyFile)
+	if err != nil {
+		return err
+	}
+	decode, err := Encoder.PemDecode(f)
+	if err != nil {
+		return err
+	}
+	key, err := x509.ParsePKCS1PrivateKey(decode)
+	if err != nil {
+		return err
+	}
+	pub, err := ssh.NewPublicKey(&key.PublicKey)
+	if err != nil {
+		return err
+	}
+	PrintString(ssh.MarshalAuthorizedKey(pub))
 	return err
 }
