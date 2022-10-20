1a89c903eb265d8d669186a16d33b3f4285f6b7f
Ricky <zeyanlin@outlook.com>
2022-10-20 13:03:31 +0800

    [test]: Disable os check for stat and tree

--- test/stat_test.go
+++ test/stat_test.go
@@ -5,13 +5,9 @@ import (
 	"testing"
 
 	"github.com/linzeyan/ops-cli/cmd"
-	"github.com/linzeyan/ops-cli/cmd/validator"
 )
 
 func TestStatBinary(t *testing.T) {
-	if validator.IsWindows() {
-		return
-	}
 	const subCommand = cmd.CommandStat
 	t.Run(subCommand, func(t *testing.T) {
 		if err := exec.Command(binaryCommand, subCommand, ".").Run(); err != nil {
